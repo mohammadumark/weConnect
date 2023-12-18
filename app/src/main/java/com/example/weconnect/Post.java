04cd4d69f0414b4c977035b29f3859ad71926a97
Mohammad Umar <128236219+mohammadumark@users.noreply.github.com>
2023-12-18 22:50:51 +0500

    second Commit

---
+++ app/src/main/java/com/example/weconnect/Post.java
@@ -0,0 +1,12 @@
+package com.example.weconnect;
+
+public class Post {
+    private String userId;
+    private String text;
+
+    // Constructors, getters, setters...
+
+    public Post() {
+        // Default constructor required for Firebase
+    }
+}
